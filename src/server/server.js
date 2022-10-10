a3ee60468c9e7a831fe773a0f0f3bf65ea958c17
james.sanders1337@gmail.com <james.sanders1337@gmail.com>
2022-10-10 15:49:40 +1000

    added: image uploading

--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const http = require("http").Server(app);
 const https = require("https");
 const fs = require("fs");
+const multer = require("multer");
 const PORT = 3000;
 const io = require("socket.io")(http, {
   cors: {
@@ -17,6 +18,70 @@ const server = require("./socket/listen.js");
 
 sockets.connect(io, PORT);
 server.listen(http, PORT);
+
+app.use(cors());
+
+// MONGOOSE
+
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/node-file-upl", {
+  useNewUrlParser: true,
+});
+var conn = mongoose.connection;
+conn.on("connected", function () {
+  console.log("database is connected successfully");
+});
+conn.on("disconnected", function () {
+  console.log("database is disconnected successfully");
+});
+conn.on("error", console.error.bind(console, "connection error:"));
+
+var imageSchema = new mongoose.Schema({
+  name: String,
+  desc: String,
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
+});
+
+// STORAGE
+const storage = multer.diskStorage({
+  destination: (req, file, callBack) => {
+    callBack(null, "uploads");
+  },
+  filename: (req, file, callBack) => {
+    callBack(null, `${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+//let upload = multer({ dest: 'uploads/' })
+
+app.post("/file", upload.single("file"), (req, res, next) => {
+  const file = req.file;
+  console.log(file.filename);
+  if (!file) {
+    const error = new Error("No File");
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+  res.send(file);
+});
+
+app.post("/multipleFiles", upload.array("files"), (req, res, next) => {
+  const files = req.files;
+  console.log(files);
+  if (!files) {
+    const error = new Error("No File");
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+  res.send({ sttus: "ok" });
+});
+
+// MONGOOSE
 
 const readUser = require("./database/read");
 /*
@@ -31,8 +96,6 @@ var getUsers = async function () {
   return Promise.all(x);
 };
 users = getUsers();
-
-app.use(cors());
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
